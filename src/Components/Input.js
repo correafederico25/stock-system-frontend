be54228d3a31c4561ac29bdd521e57fb0dbe53b6
santialbornoz1 <santi.albornoz156@gmail.com>
2020-07-15 16:01:04 -0300

    input with select

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -10,10 +10,21 @@ const handleChange = (e) => {
 } 
     return (
         <>
-    <div className="form-group mt-2">
-        <label htmlFor="">{props.textLabel}</label>
-        <input type={props.type} className="form-control" placeholder={props.placeholder} name={props.name} onChange={handleChange} />
-    </div>
+        <div className="form-group mt-2">
+          <label className="text-white" htmlFor="">{props.textLabel}</label>
+      {props.isSelectInput ? 
+            <select className="form-control" name="provider" value="" onChange={handleChange} >
+              <option default>Selecciona un proovedor</option>
+              <option value="proovedor 1">Proovedor 1</option>
+              <option value="proovedor 2">Proovedor 2</option>
+              <option value="proovedor 3">Proovedor 3</option>
+              <option value="proovedor 4">Proovedor 4</option>
+            </select>
+
+      :
+          <input type={props.type} className="form-control" placeholder={props.placeholder} name={props.name} onChange={handleChange} />
+      }
+      </div>
         </>
     )
 }
--- src/Pages/AddUsers/AddUsers.js
+++ src/Pages/AddUsers/AddUsers.js
@@ -48,55 +48,19 @@ const handleChangeInput = (e) =>{
 
           </div>
           <div className="row d-flex justify-content-center">
-            <div className="col-5 background-black border-radius-20px mr-4 ml-4">
+            <div className="col-6 background-colorSideBar border-radius-20px mr-4 ml-4">
               <form>
                 <div className="row d-flex w-100 justify-content-center pt-4">
-                  <h4 className="color-white">REGISTRAR EMPLEADO</h4>
+                  <h4 className="text-white">REGISTRAR EMPLEADO</h4>
                 </div>
-                <Input textLabel={"Prueba"} type={"text"} placeholder={"Placeholder"} name={"name"} handleChangeInput={handleChangeInput}/>
-                <Input textLabel={"Prueba"} type={"date"} placeholder={"Placeholder"} name={"lastname"} handleChangeInput={handleChangeInput}/>
-                {/* <div className="form-group mt-2">
-                  <label htmlFor="exampleInputEmail1">Nombre</label>
-                  <input type="text" className={classNameName} placeholder="Ingresa tu nombre" value={dataEmployee.name} name="name" onChange={(e) => onChangeFormEmployee(e)} />
-                  <div className="invalid-feedback">
-                    Minimo 4 caracteres.
-                </div>
-                </div>
-                <div className="form-group">
-                  <label htmlFor="exampleInputPassword1">Apellido</label>
-                  <input type="text" className={classNameLastname} placeholder="Ingresa tu apellido" value={dataEmployee.lastname} name="lastname" onChange={(e) => { onChangeFormEmployee(e) }} />
-                  <div className="invalid-feedback">
-                    Minimo 4 caracteres.
-                </div>
-                </div>
-                <div className="form-group">
-                  <label htmlFor="exampleInputPassword1">DNI</label>
-                  <input type="number" className={classNameDocumentNumber} placeholder="Ingresa tu DNI" value={dataEmployee.documentNumber} name="documentNumber" onChange={(e) => onChangeFormEmployee(e)} />
-                  <div className="invalid-feedback">
-                    Ingrese un documento valido.
-                </div>
-                </div>
-                <div className="form-group">
-                  <label htmlFor="exampleInputPassword1">Fecha de nacimiento</label>
-                  <input type="date" className={classNameDateOfBirth} placeholder="Ingresa tu fecha de nacimiento" value={dataEmployee.dateOfBirth} name="dateOfBirth" onChange={(e) => onChangeFormEmployee(e)} />
-                  <div className="invalid-feedback">
-                    Ingrese su fecha de nacimiento.
-                </div>
-                </div>
-                <div className="form-group">
-                  <label htmlFor="exampleInputPassword1">Edad</label>
-                  <input type="number" className={classNameAge} placeholder="Ingresa tu edad" value={dataEmployee.age} name="age" onChange={(e) => onChangeFormEmployee(e)} />
-                  <div className="invalid-feedback">
-                    Ingrese una edad válida.
-                </div>
-                </div>
-                <div className="form-group">
-                  <label htmlFor="exampleInputPassword1">Id / cédula</label>
-                  <input type="number" className={classNameId} placeholder="Ingresa id/cédula" value={dataEmployee.id} name="id" onChange={(e) => onChangeFormEmployee(e)} />
-                  <div className="invalid-feedback">
-                    Ingrese una cédula.
-                </div>
-                </div> */}
+                <Input textLabel={"Nombre"} isSelectInput={false} type={"text"} placeholder={"Ingresá el nombre"} name={"name"} handleChangeInput={handleChangeInput}/>
+                <Input textLabel={"Apellido"} isSelectInput={false} type={"text"} placeholder={"Ingresá el apellido"} name={"lastname"} handleChangeInput={handleChangeInput}/>
+                <Input textLabel={"DNI"} isSelectInput={false} type={"number"} placeholder={"Ingresá el DNI"} name={"lastname"} handleChangeInput={handleChangeInput}/>
+                <Input textLabel={"Email"} isSelectInput={false} type={"text"} placeholder={"Ingresá el email"} name={"email"} handleChangeInput={handleChangeInput}/>
+                <Input textLabel={"Fecha de entrada"} isSelectInput={false} type={"date"} placeholder={"Elegí la fecha de entrada"} name={"dateOfAdmission"} handleChangeInput={handleChangeInput}/>
+                <Input textLabel={"Celular"} isSelectInput={false} type={"number"} placeholder={"Ingresá el numero de celular"} name={"phoneNumber"} handleChangeInput={handleChangeInput}/>
+                <Input textLabel={"Contraseña"} isSelectInput={false} type={"password"} placeholder={"Ingresá la contraseña"} name={"password"} handleChangeInput={handleChangeInput}/>
+                <Input textLabel={"Rol"} isSelectInput={true} placeholder={"Ingresá el rol"} name={"role"} handleChangeInput={handleChangeInput}/>
                 <button type="button" className="btn btn-warning mt-3 mb-3" onClick={() => onRegisterEmployee()}>Registrar empleado</button>
               </form>
             </div>
