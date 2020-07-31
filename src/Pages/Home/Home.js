3bb842555781e56d9939a0d05554253526eca9d0
santialbornoz1 <santi.albornoz156@gmail.com>
2020-07-31 11:29:08 -0300

    probing urlback and front 2

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -7,6 +7,7 @@ const Home = (props) => {
             <Layout>
              <h1>Bienvenido/a {props.nameUser}</h1>
              <div className="mt-4"> <h3><i class="fas fa-exclamation-triangle"></i> Página en construcción</h3></div>
+             <div className="mt-4"> <h3><i class="fas fa-exclamation-triangle"></i> Ponete las pilas FEDERICO!!</h3></div>
             </Layout>
         </>
     )
